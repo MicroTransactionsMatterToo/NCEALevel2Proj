2dffaf08749238c880b1054231fc6774181b7115
MicroTransactionsMatterToo <ennisbaradine@gmail.com>
2016-03-04 20:38:02 +1300

    Fix - sizeof(envp) to sizeof(*envp)  - UmbrellaOrder.cpp
    Issue - Compiler ignores all templates for regex_search - UmbrellaOrder.cpp

--- UmbrellaOrder.cpp
+++ UmbrellaOrder.cpp
@@ -23,16 +23,23 @@ void orderToList() {
 void userInput() {
 
 }
+
 // Main Function
 int main(int argc, char *argv[], char *envp[]) {
     // Greeting
     // Debugging fo now
-    for (int i = 0; i < sizeof(envp); i++) {
+    for (int i = 0; i < sizeof(*envp); i++) {
         // Store regexx for matching
         std::regex userMatch("USER=");
         // Check if it's the USER entry of path
-        if (std::regex_search(envp[i], userMatch)) {
-            std::cout << envp[i] << std::endl;
+        if (std::regex_search(envp[i], userMatch) == true) {
+            // Store the regex cause otherwise it breaks
+            std::regex notNAME("[^NAME=]");
+            // Filter out the user's name and store it
+            char **envUserName = &envp[i];
+            std::smatch match;
+            std::regex_search(envUserName, match, notNAME);
+            std::cout << match.str() << std::endl;
         }
     }
 }

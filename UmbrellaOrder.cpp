be152f5c1caecf4431f298ceb168d5c7296021a2
MicroTransactionsMatterToo <ennisbaradine@gmail.com>
2016-03-04 16:57:21 +1300

    Add namespace - UmbrellaOrder.cpp - lines 6 - 13

--- UmbrellaOrder.cpp
+++ UmbrellaOrder.cpp
@@ -3,7 +3,14 @@
 //
 
 #include "UmbrellaOrder.h"
-
+// Namespace to store number of order and whats been previously ordered.
+namespace orderStore {
+    std::vector<char*[] /* Colour */, int /*quantity*/, int /*Order number*/>;
+    int orderNum;
+    void incrNum () {
+            orderNum += 1;
+    };
+}
 // Main Function
 int main(int argc, char *argv[]) {
 

4d39f69354fc69bf0d2ca8a079106c6f0188eb58
MicroTransactionsMatterToo <ennisbaradine@gmail.com>
2016-03-04 16:41:09 +1300

    Add main source and header Add basic includes

---
+++ UmbrellaOrder.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ennis Massey on 4/03/16.
+//
+
+#include "UmbrellaOrder.h"
---
+++ UmbrellaOrder.h
@@ -0,0 +1,10 @@
+//
+// Created by Ennis Massey on 4/03/16.
+//
+
+#ifndef PROJECT_UMBRELLAORDER_H
+#define PROJECT_UMBRELLAORDER_H
+#include <iostream>
+#include <map>
+#include <vector>
+#endif //PROJECT_UMBRELLAORDER_H
